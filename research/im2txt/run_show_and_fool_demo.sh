f62b1f1923bdc76c832aacd2ce70e7616515fe93
chenhongge <chenhg@mit.edu>
2017-12-01 21:34:02 -0500

    fix

--- research/im2txt/run_show_and_fool_demo.sh
+++ research/im2txt/run_show_and_fool_demo.sh
@@ -38,7 +38,6 @@ mkdir -p ${OUTPUT_DIR}
 source activate huanpy36
 VOCAB_FILE="im2txt/pretrained/word_counts.txt"
 CHECKPOINT_PATH="im2txt/pretrained/model2.ckpt-2000000"
-CAPTION_FILE="/home/hongge/im2txt/EasyCocoEval/coco-caption/annotations/captions_val2014.json"
 bazel build -c opt im2txt/show_and_fool_demo
 set -x
 CUDA_VISIBLE_DEVICES=${GPU_number} bazel-bin/im2txt/show_and_fool_demo --checkpoint_path=${CHECKPOINT_PATH} --vocab_file=${VOCAB_FILE}   --attack_filepath=${ATTACK_FILEPATH} --target_filepath=${TARGET_FILEPATH} --use_logits=True --result_directory="${OUTPUT_DIR}" --norm="l2" -seed=8 --C=1 "${PARAMS[@]}" 2>&1|tee ${OUTPUT_DIR}/log.txt
