2ca7a8e17fa8f1a3528e74f5e716a9ee08150fe9
Huan Zhang <ecezhang@ucdavis.edu>
2017-12-05 03:28:28 -0500

    add auto model downloading script

---
+++ research/im2txt/download_model.sh
@@ -0,0 +1,4 @@
+wget -O show-and-tell_pretrained.tar.bz2 "http://jaina.cs.ucdavis.edu/datasets/adv/captioning/show-and-tell_pretrained.tar.bz2"
+tar xvf show-and-tell_pretrained.tar.bz2 -C im2txt
+rm show-and-tell_pretrained.tar.bz2
+
