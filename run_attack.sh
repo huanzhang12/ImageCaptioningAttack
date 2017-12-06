08064cdf8c40c65f8b6895901a18c5d08967ca9e
chenhongge <32213009+chenhongge@users.noreply.github.com>
2017-12-05 21:17:12 -0500

    Update run_attack.sh

--- run_attack.sh
+++ run_attack.sh
@@ -27,18 +27,8 @@ echo Additional parameters: ${PARAMS[@]}
 
 mkdir -p ${OUTPUT_DIR}
 
-# if [ "$(arch)" = "x86_64" ]; then
-#    source activate tensorflow
-# fi
-# if [ "$(arch)" = "ppc64le" ]; then
-#    source ${HOME}/.bashrc
-#    source activate huanpy36
-# fi
-source activate huanpy36 # activate your tensorflow environment
 VOCAB_FILE="im2txt/pretrained/word_counts.txt"
 CHECKPOINT_PATH="im2txt/pretrained/model2.ckpt-2000000"
-# CAPTION_FILE="/dccstor/dloptdata1/im2txt/data/mscoco/raw-data/annotations/captions_val2014.json"
-# IMAGE_DIRECTORY="/dccstor/dloptdata1/im2txt/data/mscoco/raw-data/val2014/"
 CAPTION_FILE="/home/hongge/im2txt/EasyCocoEval/coco-caption/annotations/captions_val2014.json"
 IMAGE_DIRECTORY="/data2/mscoco/image/val2014/"
 bazel build -c opt im2txt/run_attack_BATCH_search_C
@@ -46,5 +36,5 @@ set -x
 bazel-bin/im2txt/run_attack_BATCH_search_C --checkpoint_path=${CHECKPOINT_PATH}  --caption_file=${CAPTION_FILE} --vocab_file=${VOCAB_FILE}   --input_files=${IMAGE_FILE} --image_directory=${IMAGE_DIRECTORY} --use_logits=True --exp_num=${NUM_ATTACKS} --offset=${OFFSET} --result_directory="${OUTPUT_DIR}" --norm="l2" -seed=8 --C=1 "${PARAMS[@]}" 2>&1|tee ${OUTPUT_DIR}/log_$OFFSET.txt
 set +x
 rm -r bazel-*
-source deactivate
+
 
